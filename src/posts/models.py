eafe9d9f46d8b8a4380470f6db2c4df7425ee0c6
Tony Wittinger <anwittin@gmail.com>
2017-04-15 15:38:50 -0700

    adding project

---
+++ src/posts/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    slug = models.CharField()
+
+    def __str__(self):
+        return self.title
