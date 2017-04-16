96c3547f4d6da7d339d0fe6a61443b69ca8d9a9b
Tony Wittinger <anwittin@gmail.com>
2017-04-16 15:22:55 -0700

    database rebuild and template changes

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
 
 from .models import Post
 from .forms import PostForm
@@ -8,14 +9,12 @@ from .forms import PostForm
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
         messages.success(request, "Created Successfully")
         return HttpResponseRedirect(instance.get_absolute_url())
-    else:
-        messages.error(request, "Failed to Create")
     context = {
         "form": form,
     }
@@ -31,16 +30,28 @@ def post_detail(request, id=None):
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5) # Show 5 contacts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "object_list": queryset,
-        "title": "list"
-    }
-    return render(request, "index.html", context)
+        "title": "list",
+        "page_request_var": page_request_var
+            }
+    return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
